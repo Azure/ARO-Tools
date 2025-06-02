772048692112460e4dc93fba78d64ed03331eb97
Jan-Hendrik Boll <jboll@redhat.com>
2025-06-02 13:18:15 +0000

    Moved this code back to ARO-Pipelines

--- pkg/types/variables.go
+++ pkg/types/variables.go
@@ -14,11 +14,6 @@
 
 package types
 
-import (
-	"fmt"
-	"strings"
-)
-
 // Variable
 // Use this to pass in values to pipeline steps. Values can come from various sources:
 //   - Value: Use the value field to "hardcode" a value.
@@ -37,49 +32,4 @@ type Variable struct {
 type Input struct {
 	Name string `yaml:"name"`
 	Step string `yaml:"step"`
-}
-
-// Validate checks if the variable is valid
-// Parameters:
-//   - data: The configuration used to validate the Variable
-//
-// Returns:
-//   - error: error in case there is any
-func (v *Variable) Validate(data map[string]any) error {
-	if v.ConfigRef != "" {
-		val, err := getConfigValue(data, v.ConfigRef)
-		if err != nil {
-			return err
-		}
-
-		v.Value = val
-	} else if v.Value == nil && v.Input == nil {
-		// In this case no value is set, clearly an error
-		return fmt.Errorf("missing or empty value for variable %s", v.Name)
-	}
-
-	return nil
-}
-
-// getConfigValue returns the value for the key in the configuration
-func getConfigValue(data map[string]any, key string) (any, error) {
-	keys := strings.Split(key, ".")
-
-	var result any = data
-
-	for i, k := range keys {
-		cast, matchesType := result.(map[string]any)
-		if !matchesType {
-			return nil, fmt.Errorf("%s: configuration value is %T, not %T", strings.Join(keys[0:i], "."), result, map[string]any{})
-		}
-
-		value, exists := cast[k]
-		if !exists {
-			return nil, fmt.Errorf("%s: configuration has no member %s", strings.Join(keys[0:i], "."), k)
-		}
-
-		result = value
-	}
-
-	return result, nil
 }
