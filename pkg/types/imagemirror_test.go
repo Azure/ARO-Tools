cc0fb0f3295700168e378158763a519214c116d9
Gerd Oberlechner <goberlec@redhat.com>
2025-06-30 23:32:16 +0200

    ImageMirror step name fix
    the resulting shell step name of an image mirror step was hardcoded to `image-mirror`, which prevented step dependencies to resolve properly within EV2.
    this PR uses the actuall image mirror step name as the shell step name.
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

---
+++ pkg/types/imagemirror_test.go
@@ -0,0 +1,58 @@
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types
+
+import (
+	"testing"
+
+	"github.com/Azure/ARO-Tools/internal/testutil"
+)
+
+func TestResolveImageMirrorStep(t *testing.T) {
+	tests := []struct {
+		name       string
+		input      ImageMirrorStep
+		scriptFile string
+	}{
+		{
+			name: "image-mirror-step",
+			input: ImageMirrorStep{
+				StepMeta: StepMeta{
+					Name:   "image-mirror-step",
+					Action: "ImageMirror",
+				},
+				TargetACR:          Value{Value: "myacr.azurecr.io"},
+				SourceRegistry:     Value{Value: "docker.io"},
+				Repository:         Value{Value: "nginx"},
+				Digest:             Value{Value: "sha256:123456"},
+				PullSecretKeyVault: Value{Value: "my-keyvault"},
+				PullSecretName:     Value{Value: "my-pull-secret"},
+				ShellIdentity:      Value{Value: "my-identity"},
+			},
+			scriptFile: "/path/to/script.sh",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := ResolveImageMirrorStep(tt.input, tt.scriptFile)
+			if err != nil {
+				t.Fatalf("ResolveImageMirrorStep() error = %v", err)
+			}
+
+			testutil.CompareWithFixture(t, result, testutil.WithExtension(".yaml"))
+		})
+	}
+}
