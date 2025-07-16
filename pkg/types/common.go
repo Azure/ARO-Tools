b87a8b4c6c145871c58e330d41d762b8f5da36d9
Jonathan Chang <jonachang@microsoft.com>
2025-07-16 10:46:07 -0700

    add kusto step for billing infra

--- pkg/types/common.go
+++ pkg/types/common.go
@@ -137,3 +137,14 @@ type FeatureRegistrationStep struct {
 func (s *FeatureRegistrationStep) Description() string {
 	return fmt.Sprintf("Step %s\n Kind: %s\n", s.Name, s.Action)
 }
+
+type KustoStep struct {
+	SecretId         Value `json:"secretId,omitempty"`
+	ApplicationId    Value `json:"applicationId,omitempty"`
+	ConnectionString Value `json:"connectionString,omitempty"`
+	Command          Value `json:"command,omitempty"`
+}
+
+func (s *KustoStep) Description() string {
+	return fmt.Sprintf("Step %s\n Kind: %s\n", s.Name, s.Action)
+}
