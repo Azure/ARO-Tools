bddd190f1cb547a9341aae33af2fbe92e50cb406
Steve Kuznetsov <stekuznetsov@microsoft.com>
2025-07-18 10:35:44 -0600

    pkg/seret-sync: use KEK/DEK for encryption of data
    Signed-off-by: Steve Kuznetsov <stekuznetsov@microsoft.com>

--- pkg/secret-sync/config/config.go
+++ pkg/secret-sync/config/config.go
@@ -10,16 +10,28 @@ import (
 type SecretSync struct {
 	// KeyVaults stores the data for each key vault keyed by the connection URI, like "my-key-vault.azure-test.net".
 	// As the connection URI contains a tenant-unique name and a tenant-specific domain, these are globally unique.
-	KeyVaults map[string]KeyVault `yaml:"keyVaults"`
+	KeyVaults map[string]KeyVault `json:"keyVaults"`
 }
 
 // KeyVault holds encrypted data that should be populated into a key vault and the public key used to encrypt these data.
 type KeyVault struct {
+	// KeyEncryptionKey holds the public half of the key encryption key in PEM block format.
+	KeyEncryptionKey string `json:"keyEncryptionKey"`
+
 	// EncryptedSecrets stores Azure Key Vault secret data by secret name that should be populated into the KeyVault.
 	// These data are base64-encoded to be easy to embed into this text-based data format.
-	EncryptedSecrets map[string]string `yaml:"encryptedSecrets"`
-	// PublicKeyData holds the PKIX public key data in PEM block format.
-	PublicKeyData string `yaml:"publicKeyData"`
+	EncryptedSecrets map[string]EncryptedSecret `json:"encryptedSecrets"`
+}
+
+type EncryptedSecret struct {
+	// DataEncryptionKey holds the key used to encrypt the data; the key is itself encrypted using the key encryption key.
+	// This key may only be used once; the lifetime of the key is the same as the lifetime of the data being encrypted.
+	// This encrypted data is base64-encoded to be easy to embed into this text-based data format.
+	DataEncryptionKey string `json:"dataEncryptionKey"`
+
+	// Data holds the encrypted secret data that needs to be populated into KeyVault.
+	// This encrypted data is base64-encoded to be easy to embed into this text-based data format.
+	Data string `json:"data"`
 }
 
 func Load(path string) (*SecretSync, error) {
