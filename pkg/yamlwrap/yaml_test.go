427649a57c476be517acfd3bd6ca5dca1aaad631
Gerd Oberlechner <goberlec@redhat.com>
2025-07-24 18:30:32 +0200

    feat: add yamlwrap package for quoting unquoted Go templates in YAML (#52)
    * yamlwrap implementation in go
    
    * yaml multline testcases
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>
    
    * validate wrapping creates valid yaml
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>
    
    * remove --inplace
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>
    
    * simplify line matching logic - making special array handling obsolete
    
    * wrap object array
    
    * cli flag to skip validation after wrap
    
    * simplify template pattern
    
    * simplify wrap logic
    
    * value quote handling
    
    ---------
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

---
+++ pkg/yamlwrap/yaml_test.go
@@ -0,0 +1,84 @@
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package yamlwrap
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/Azure/ARO-Tools/internal/testutil"
+)
+
+func TestWrapYAML(t *testing.T) {
+	inputPath := filepath.Join("testdata", "unwrapped.yaml")
+	data, err := os.ReadFile(inputPath)
+	require.NoError(t, err)
+
+	result, err := WrapYAML(data, true)
+	require.NoError(t, err)
+
+	testutil.CompareWithFixture(t, result, testutil.WithExtension(".yaml"))
+}
+
+func TestUnwrapYAML(t *testing.T) {
+	inputPath := filepath.Join("testdata", "wrapped.yaml")
+	data, err := os.ReadFile(inputPath)
+	require.NoError(t, err)
+
+	result, err := UnwrapYAML(data)
+	require.NoError(t, err)
+
+	testutil.CompareWithFixture(t, result, testutil.WithExtension(".yaml"))
+}
+
+func TestRoundTrip(t *testing.T) {
+	// Read the unwrapped.yaml file
+	inputPath := filepath.Join("testdata", "unwrapped.yaml")
+	original, err := os.ReadFile(inputPath)
+	require.NoError(t, err)
+
+	// wrap it in memory
+	wrapped, err := WrapYAML(original, true)
+	require.NoError(t, err)
+
+	// ... then unwrap
+	result, err := UnwrapYAML(wrapped)
+	require.NoError(t, err)
+
+	// Compare that the round trip produces the same result as the original
+	assert.Empty(t, cmp.Diff(original, result))
+}
+
+func TestWrapValidation(t *testing.T) {
+	// Test that validation errors are emitted for invalid YAML when validation is enabled
+	invalidYAML := []byte(`
+key: value
+invalid: [unclosed array
+another: normal value
+`)
+
+	// This should fail when validation is enabled
+	_, err := WrapYAML(invalidYAML, true)
+	require.Error(t, err)
+
+	// This should succeed when validation is disabled
+	_, err = WrapYAML(invalidYAML, false)
+	require.NoError(t, err)
+}
