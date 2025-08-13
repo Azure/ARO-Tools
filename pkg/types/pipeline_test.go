bdcd0f2852bfc3f92016c047c91439ab05fcce7a
Weinong Wang <weinong@outlook.com>
2025-08-13 11:19:26 -0700

    fixed the schema for subscription provisioning

--- pkg/types/pipeline_test.go
+++ pkg/types/pipeline_test.go
@@ -54,3 +54,183 @@ func TestNewPipelineFromFile(t *testing.T) {
 
 	testutil.CompareWithFixture(t, pipeline, testutil.WithExtension(".yaml"))
 }
+
+func TestSubscriptionProvisioningSchemaValidation(t *testing.T) {
+	tests := []struct {
+		name        string
+		yaml        string
+		expectError bool
+		description string
+	}{
+		{
+			name: "backfill_only",
+			yaml: `
+$schema: pipeline.schema.v1
+serviceGroup: Microsoft.Azure.ARO.Test
+rolloutName: Test Rollout
+resourceGroups:
+- name: test
+  resourceGroup: test-rg
+  subscription: test-sub
+  subscriptionProvisioning:
+    backfillSubscriptionId:
+      value: existing-sub-123
+  steps:
+  - name: deploy
+    action: Shell
+    command: echo hello
+`,
+			expectError: false,
+			description: "backfillSubscriptionId alone should be valid",
+		},
+		{
+			name: "new_subscription_only",
+			yaml: `
+$schema: pipeline.schema.v1
+serviceGroup: Microsoft.Azure.ARO.Test
+rolloutName: Test Rollout
+resourceGroups:
+- name: test
+  resourceGroup: test-rg
+  subscription: test-sub
+  subscriptionProvisioning:
+    displayName:
+      value: "New Subscription"
+    roleAssignment: param/foo.bicepparam
+  steps:
+  - name: deploy
+    action: Shell
+    command: echo hello
+`,
+			expectError: false,
+			description: "displayName and roleAssignment alone should be valid",
+		},
+		{
+			name: "backfill_with_display_name",
+			yaml: `
+$schema: pipeline.schema.v1
+serviceGroup: Microsoft.Azure.ARO.Test
+rolloutName: Test Rollout
+resourceGroups:
+- name: test
+  resourceGroup: test-rg
+  subscription: test-sub
+  subscriptionProvisioning:
+    backfillSubscriptionId:
+      value: existing-sub-123
+    displayName:
+      value: "My Subscription"
+  steps:
+  - name: deploy
+    action: Shell
+    command: echo hello
+`,
+			expectError: false,
+			description: "backfillSubscriptionId with displayName should be valid (new behavior)",
+		},
+		{
+			name: "backfill_with_all_fields",
+			yaml: `
+$schema: pipeline.schema.v1
+serviceGroup: Microsoft.Azure.ARO.Test
+rolloutName: Test Rollout
+resourceGroups:
+- name: test
+  resourceGroup: test-rg
+  subscription: test-sub
+  subscriptionProvisioning:
+    backfillSubscriptionId:
+      value: existing-sub-123
+    displayName:
+      value: "My Subscription"
+    roleAssignment: param/foo.bicepparam
+    airsRegisteredUserPrincipalId:
+      value: "user-123"
+    certificateDomains:
+      value: "*.example.com"
+  steps:
+  - name: deploy
+    action: Shell
+    command: echo hello
+`,
+			expectError: false,
+			description: "backfillSubscriptionId with all other fields should be valid (new behavior)",
+		},
+		{
+			name: "missing_required_fields",
+			yaml: `
+$schema: pipeline.schema.v1
+serviceGroup: Microsoft.Azure.ARO.Test
+rolloutName: Test Rollout
+resourceGroups:
+- name: test
+  resourceGroup: test-rg
+  subscription: test-sub
+  subscriptionProvisioning:
+    airsRegisteredUserPrincipalId:
+      value: "user-123"
+  steps:
+  - name: deploy
+    action: Shell
+    command: echo hello
+`,
+			expectError: true,
+			description: "missing both required patterns should fail validation",
+		},
+		{
+			name: "display_name_without_role_assignment",
+			yaml: `
+$schema: pipeline.schema.v1
+serviceGroup: Microsoft.Azure.ARO.Test
+rolloutName: Test Rollout
+resourceGroups:
+- name: test
+  resourceGroup: test-rg
+  subscription: test-sub
+  subscriptionProvisioning:
+    displayName:
+      value: "New Subscription"
+  steps:
+  - name: deploy
+    action: Shell
+    command: echo hello
+`,
+			expectError: true,
+			description: "displayName without roleAssignment should fail validation",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			region := "uksouth"
+			regionShort := "uks"
+			stamp := "1"
+			cloud := "public"
+			environment := "int"
+
+			ev2, err := ev2config.ResolveConfig(cloud, region)
+			require.NoError(t, err)
+			provider, err := config.NewConfigProvider("../../testdata/config.yaml")
+			require.NoError(t, err)
+			resolver, err := provider.GetResolver(&config.ConfigReplacements{
+				RegionReplacement:      region,
+				RegionShortReplacement: regionShort,
+				StampReplacement:       stamp,
+				CloudReplacement:       cloud,
+				EnvironmentReplacement: environment,
+				Ev2Config:              ev2,
+			})
+			require.NoError(t, err)
+
+			cfg, err := resolver.GetRegionConfiguration(region)
+			require.NoError(t, err)
+
+			_, err = NewPipelineFromBytes([]byte(tt.yaml), cfg)
+			if tt.expectError {
+				assert.Error(t, err, tt.description)
+			} else {
+				assert.NoError(t, err, tt.description)
+			}
+		})
+	}
+}
