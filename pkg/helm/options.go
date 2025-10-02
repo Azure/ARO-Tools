8b5e8e7cf1a1a5dd94187ec72d62ac0afda2f956
Steve Kuznetsov <stekuznetsov@microsoft.com>
2025-10-02 10:29:57 -0600

    pkg/helm: expose input values
    Signed-off-by: Steve Kuznetsov <stekuznetsov@microsoft.com>

--- pkg/helm/options.go
+++ pkg/helm/options.go
@@ -241,6 +241,8 @@ func (opts *Options) Deploy(ctx context.Context) error {
 		return fmt.Errorf("failed to create logger: %w", err)
 	}
 
+	logger.Info("Resolved input values.", "values", opts.Values)
+
 	logger.Info("Applying namespaces.")
 	// Helm does not let us manage namespaces easily, so we need to apply them ourselves, up-front.
 	for _, namespace := range opts.Namespaces {
