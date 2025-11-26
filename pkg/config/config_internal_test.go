6069cbd17985a60af516eb0d932f048290267bfe
Gerd Oberlechner <goberlec@redhat.com>
2025-11-26 23:13:36 +0100

    config: validate schema fields don't collide with ev2 config
    add validation in GetResolver to detect when config schemas define
    properties that conflict with fields from the sanitized ev2 config
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- pkg/config/config_internal_test.go
+++ pkg/config/config_internal_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/Azure/ARO-Tools/pkg/config/ev2config"
 )
 
 func TestValidateSchema(t *testing.T) {
@@ -23,4 +25,25 @@ func TestValidateSchema(t *testing.T) {
 
 	validationErr := resolver.ValidateSchema(cfg)
 	require.NoError(t, validationErr)
+}
+
+func TestValidateEV2SchemaCollisionDetection(t *testing.T) {
+	provider, err := NewConfigProvider("./testdata/config.with-ev2-conflicts.yaml")
+	require.NoError(t, err)
+
+	ev2Cfg, err := ev2config.ResolveConfig("public", "uksouth")
+	require.NoError(t, err)
+
+	// GetResolver should fail because the schema has a collision with geoShortId
+	_, err = provider.GetResolver(&ConfigReplacements{
+		CloudReplacement:       "public",
+		EnvironmentReplacement: "int",
+		RegionReplacement:      "uksouth",
+		RegionShortReplacement: "ln",
+		Ev2Config:              ev2Cfg,
+	})
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "collide with reserved ev2 config fields")
+	require.Contains(t, err.Error(), "geoShortId")
+	require.Contains(t, err.Error(), "config.schema.with-ev2-conflicts.json")
 }
