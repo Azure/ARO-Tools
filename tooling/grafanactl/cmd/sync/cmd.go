Azure/ARO-HCP

Sync grafana dashboards from git

tooling/grafanactl/cmd/sync/cmd.go
@@ -1,0 +1,95 @@
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sync
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-logr/logr"
+	"github.com/spf13/cobra"
+
+	"github.com/Azure/ARO-HCP/tooling/grafanactl/internal/grafana"
+)
+
+const dashboardsGroupID = "dashboards"
+
+func NewSyncCommand(group string) (*cobra.Command, error) {
+	opts := DefaultSyncDashboardsOptions()
+
+	syncCmd := &cobra.Command{
+		Use:     "sync",
+		Short:   "Sync Grafana resources",
+		Long:    "Sync Grafana resources as per git",
+		GroupID: group,
+	}
+
+	syncCmd.AddGroup(&cobra.Group{
+		ID:    dashboardsGroupID,
+		Title: "Sync Commands:",
+	})
+
+	syncDashboardsCmd := &cobra.Command{
+		Use:     "dashboards",
+		Short:   "Sync Grafana dashboards",
+		Long:    "Sync Grafana dashboards and folders present in git, remove stale dashboards",
+		GroupID: dashboardsGroupID,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return opts.Run(cmd.Context())
+		},
+	}
+
+	if err := BindSyncDashboardsOptions(opts, syncDashboardsCmd); err != nil {
+		return nil, err
+	}
+
+	syncCmd.AddCommand(syncDashboardsCmd)
+
+	return syncCmd, nil
+}
+
+func (opts *RawSyncDashboardsOptions) Run(ctx context.Context) error {
+	validated, err := opts.Validate(ctx)
+	if err != nil {
+		return fmt.Errorf("validation failed: %w", err)
+	}
+
+	completed, err := validated.Complete(ctx)
+	if err != nil {
+		return fmt.Errorf("completion failed: %w", err)
+	}
+
+	return completed.Run(ctx)
+}
+
+func (o *CompletedSyncDashboardsOptions) Run(ctx context.Context) error {
+	logger := logr.FromContextOrDiscard(ctx)
+
+	logger.Info("Starting dashboard sync", "dry-run", o.DryRun)
+
+	syncer := grafana.NewDashboardSyncer(
+		o.GrafanaClient,
+		o.Config,
+		o.ConfigPath,
+		o.DryRun,
+	)
+
+	if err := syncer.Sync(ctx); err != nil {
+		return fmt.Errorf("sync failed: %w", err)
+	}
+
+	logger.Info("Dashboard sync completed successfully")
+	return nil
+}

tooling/grafanactl/cmd/sync/options.go
@@ -1,0 +1,130 @@
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sync
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/Azure/ARO-HCP/tooling/grafanactl/cmd/base"
+	"github.com/Azure/ARO-HCP/tooling/grafanactl/internal/azure"
+	"github.com/Azure/ARO-HCP/tooling/grafanactl/internal/config"
+	"github.com/Azure/ARO-HCP/tooling/grafanactl/internal/grafana"
+	"github.com/Azure/ARO-Tools/pkg/cmdutils"
+	"github.com/spf13/cobra"
+)
+
+// RawSyncDashboardsOptions represents the initial, unvalidated configuration for sync operations.
+type RawSyncDashboardsOptions struct {
+	*base.BaseOptions
+	DryRun              bool
+	ConfigPath string
+}
+
+// validatedSyncDashboardsOptions is a private struct that enforces the options validation pattern.
+type validatedSyncDashboardsOptions struct {
+	*RawSyncDashboardsOptions
+}
+
+// ValidatedSyncDashboardsOptions represents sync configuration that has passed validation.
+type ValidatedSyncDashboardsOptions struct {
+	// Embed a private pointer that cannot be instantiated outside of this package
+	*validatedSyncDashboardsOptions
+}
+
+// CompletedSyncDashboardsOptions represents the final, fully validated and initialized configuration
+// for sync operations.
+type CompletedSyncDashboardsOptions struct {
+	*validatedSyncDashboardsOptions
+	GrafanaClient *grafana.Client
+	Config        *config.ObservabilityConfig
+}
+
+// DefaultSyncDashboardsOptions returns a new RawSyncDashboardsOptions with default values
+func DefaultSyncDashboardsOptions() *RawSyncDashboardsOptions {
+	return &RawSyncDashboardsOptions{
+		BaseOptions: base.DefaultBaseOptions(),
+		DryRun:      false,
+	}
+}
+
+// BindSyncDashboardsOptions binds command-line flags to the options
+func BindSyncDashboardsOptions(opts *RawSyncDashboardsOptions, cmd *cobra.Command) error {
+	if err := base.BindBaseOptions(opts.BaseOptions, cmd); err != nil {
+		return err
+	}
+
+	flags := cmd.Flags()
+	flags.BoolVar(&opts.DryRun, "dry-run", false, "Perform a dry run without making changes")
+	flags.StringVar(&opts.ConfigPath, "config-file", "", "Config file with references to grafana dashboards (required)")
+
+	_ = cmd.MarkFlagRequired("config-file")
+	return nil
+}
+
+// Validate performs validation on the raw options
+func (o *RawSyncDashboardsOptions) Validate(ctx context.Context) (*ValidatedSyncDashboardsOptions, error) {
+	if err := base.ValidateBaseOptions(o.BaseOptions); err != nil {
+		return nil, err
+	}
+
+	absPath, err := filepath.Abs(o.ConfigPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to resolve config file path: %w", err)
+	}
+
+	if _, err := os.Stat(absPath); err != nil {
+		return nil, fmt.Errorf("config file not found: %w", err)
+	}
+
+	o.ConfigPath = absPath
+
+	return &ValidatedSyncDashboardsOptions{
+		validatedSyncDashboardsOptions: &validatedSyncDashboardsOptions{
+			RawSyncDashboardsOptions: o,
+		},
+	}, nil
+}
+
+// Complete performs final initialization to create fully usable sync options.
+func (o *ValidatedSyncDashboardsOptions) Complete(ctx context.Context) (*CompletedSyncDashboardsOptions, error) {
+	cfg, err := config.LoadFromFile(o.ConfigPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load config: %w", err)
+	}
+
+	cred, err := cmdutils.GetAzureTokenCredentials()
+	if err != nil {
+		return nil, fmt.Errorf("failed to obtain Azure credentials: %w", err)
+	}
+
+	managedGrafanaClient, err := azure.NewManagedGrafanaClient(o.SubscriptionID, cred)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create managed Grafana client: %w", err)
+	}
+
+	grafanaClient, err := grafana.NewClient(ctx, cred, managedGrafanaClient, o.SubscriptionID, o.ResourceGroup, o.GrafanaName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Grafana client: %w", err)
+	}
+
+	return &CompletedSyncDashboardsOptions{
+		validatedSyncDashboardsOptions: o.validatedSyncDashboardsOptions,
+		GrafanaClient:                  grafanaClient,
+		Config:                         cfg,
+	}, nil
+}

Azure/ARO-Tools

tooling/grafanactl/cmd/sync/cmd.go
@@ -1,0 +1,95 @@
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sync
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-logr/logr"
+	"github.com/spf13/cobra"
+
+	"github.com/Azure/ARO-HCP/tooling/grafanactl/internal/grafana"
+)
+
+const dashboardsGroupID = "dashboards"
+
+func NewSyncCommand(group string) (*cobra.Command, error) {
+	opts := DefaultSyncDashboardsOptions()
+
+	syncCmd := &cobra.Command{
+		Use:     "sync",
+		Short:   "Sync Grafana resources",
+		Long:    "Sync Grafana resources as per git",
+		GroupID: group,
+	}
+
+	syncCmd.AddGroup(&cobra.Group{
+		ID:    dashboardsGroupID,
+		Title: "Sync Commands:",
+	})
+
+	syncDashboardsCmd := &cobra.Command{
+		Use:     "dashboards",
+		Short:   "Sync Grafana dashboards",
+		Long:    "Sync Grafana dashboards and folders present in git, remove stale dashboards",
+		GroupID: dashboardsGroupID,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return opts.Run(cmd.Context())
+		},
+	}
+
+	if err := BindSyncDashboardsOptions(opts, syncDashboardsCmd); err != nil {
+		return nil, err
+	}
+
+	syncCmd.AddCommand(syncDashboardsCmd)
+
+	return syncCmd, nil
+}
+
+func (opts *RawSyncDashboardsOptions) Run(ctx context.Context) error {
+	validated, err := opts.Validate(ctx)
+	if err != nil {
+		return fmt.Errorf("validation failed: %w", err)
+	}
+
+	completed, err := validated.Complete(ctx)
+	if err != nil {
+		return fmt.Errorf("completion failed: %w", err)
+	}
+
+	return completed.Run(ctx)
+}
+
+func (o *CompletedSyncDashboardsOptions) Run(ctx context.Context) error {
+	logger := logr.FromContextOrDiscard(ctx)
+
+	logger.Info("Starting dashboard sync", "dry-run", o.DryRun)
+
+	syncer := grafana.NewDashboardSyncer(
+		o.GrafanaClient,
+		o.Config,
+		o.ConfigFilePath,
+		o.DryRun,
+	)
+
+	if err := syncer.Sync(ctx); err != nil {
+		return fmt.Errorf("sync failed: %w", err)
+	}
+
+	logger.Info("Dashboard sync completed successfully")
+	return nil
+}

tooling/grafanactl/cmd/sync/options.go
@@ -1,0 +1,132 @@
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sync
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+
+	"github.com/Azure/ARO-Tools/pkg/cmdutils"
+
+	"github.com/Azure/ARO-HCP/tooling/grafanactl/cmd/base"
+	"github.com/Azure/ARO-HCP/tooling/grafanactl/internal/azure"
+	"github.com/Azure/ARO-HCP/tooling/grafanactl/internal/config"
+	"github.com/Azure/ARO-HCP/tooling/grafanactl/internal/grafana"
+)
+
+// RawSyncDashboardsOptions represents the initial, unvalidated configuration for sync operations.
+type RawSyncDashboardsOptions struct {
+	*base.BaseOptions
+	DryRun         bool
+	ConfigFilePath string
+}
+
+// validatedSyncDashboardsOptions is a private struct that enforces the options validation pattern.
+type validatedSyncDashboardsOptions struct {
+	*RawSyncDashboardsOptions
+}
+
+// ValidatedSyncDashboardsOptions represents sync configuration that has passed validation.
+type ValidatedSyncDashboardsOptions struct {
+	// Embed a private pointer that cannot be instantiated outside of this package
+	*validatedSyncDashboardsOptions
+}
+
+// CompletedSyncDashboardsOptions represents the final, fully validated and initialized configuration
+// for sync operations.
+type CompletedSyncDashboardsOptions struct {
+	*validatedSyncDashboardsOptions
+	GrafanaClient *grafana.Client
+	Config        *config.ObservabilityConfig
+}
+
+// DefaultSyncDashboardsOptions returns a new RawSyncDashboardsOptions with default values
+func DefaultSyncDashboardsOptions() *RawSyncDashboardsOptions {
+	return &RawSyncDashboardsOptions{
+		BaseOptions: base.DefaultBaseOptions(),
+		DryRun:      false,
+	}
+}
+
+// BindSyncDashboardsOptions binds command-line flags to the options
+func BindSyncDashboardsOptions(opts *RawSyncDashboardsOptions, cmd *cobra.Command) error {
+	if err := base.BindBaseOptions(opts.BaseOptions, cmd); err != nil {
+		return err
+	}
+
+	flags := cmd.Flags()
+	flags.BoolVar(&opts.DryRun, "dry-run", false, "Perform a dry run without making changes")
+	flags.StringVar(&opts.ConfigFilePath, "config-file", "", "Path to config file with Grafana dashboard references (absolute or relative path, required)")
+
+	_ = cmd.MarkFlagRequired("config-file")
+	return nil
+}
+
+// Validate performs validation on the raw options
+func (o *RawSyncDashboardsOptions) Validate(ctx context.Context) (*ValidatedSyncDashboardsOptions, error) {
+	if err := base.ValidateBaseOptions(o.BaseOptions); err != nil {
+		return nil, err
+	}
+
+	absPath, err := filepath.Abs(o.ConfigFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to resolve config file path: %w", err)
+	}
+
+	if _, err := os.Stat(absPath); err != nil {
+		return nil, fmt.Errorf("config file not found: %w", err)
+	}
+
+	o.ConfigFilePath = absPath
+
+	return &ValidatedSyncDashboardsOptions{
+		validatedSyncDashboardsOptions: &validatedSyncDashboardsOptions{
+			RawSyncDashboardsOptions: o,
+		},
+	}, nil
+}
+
+// Complete performs final initialization to create fully usable sync options.
+func (o *ValidatedSyncDashboardsOptions) Complete(ctx context.Context) (*CompletedSyncDashboardsOptions, error) {
+	cfg, err := config.LoadFromFile(o.ConfigFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load config: %w", err)
+	}
+
+	cred, err := cmdutils.GetAzureTokenCredentials()
+	if err != nil {
+		return nil, fmt.Errorf("failed to obtain Azure credentials: %w", err)
+	}
+
+	managedGrafanaClient, err := azure.NewManagedGrafanaClient(o.SubscriptionID, cred)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create managed Grafana client: %w", err)
+	}
+
+	grafanaClient, err := grafana.NewClient(ctx, cred, managedGrafanaClient, o.SubscriptionID, o.ResourceGroup, o.GrafanaName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Grafana client: %w", err)
+	}
+
+	return &CompletedSyncDashboardsOptions{
+		validatedSyncDashboardsOptions: o.validatedSyncDashboardsOptions,
+		GrafanaClient:                  grafanaClient,
+		Config:                         cfg,
+	}, nil
+}